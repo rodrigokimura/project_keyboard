7e2f033b473c610313d47952a578046935090701
Rodrigo Eiti Kimura <rodrigo.kimura@catskillet.com>
2022-12-01 14:18:48 -0300

    Add support for encoder

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,18 +5,28 @@ byte cols[] = {10, 16};
 
 const int ROW_COUNT = sizeof(rows) / sizeof(rows[0]);
 const int COL_COUNT = sizeof(cols) / sizeof(cols[0]);
-const int CMD_DELAY = 100;
+const int CMD_DELAY = 10;
 const int BAUD_RATE = 115200;
 
 byte keys[COL_COUNT][ROW_COUNT];
-int commands[COL_COUNT][ROW_COUNT];
+struct KeyData
+{
+    int key;
+    bool media;
+};
+KeyData commands[COL_COUNT][ROW_COUNT];
 
-const int KEY_0_0 = KEY_LEFT_CTRL;
-const int KEY_0_1 = KEY_LEFT_SHIFT;
-const int KEY_0_2 = KEY_C;
-const int KEY_1_0 = KEY_Z;
-const int KEY_1_1 = KEY_V;
-const int KEY_1_2 = MEDIA_PLAY_PAUSE;
+const KeyData KEY_0_0 = {KEY_LEFT_CTRL, false};
+const KeyData KEY_0_1 = {KEY_LEFT_SHIFT, false};
+const KeyData KEY_0_2 = {KEY_C, false};
+const KeyData KEY_1_0 = {KEY_Z, false};
+const KeyData KEY_1_1 = {KEY_V, false};
+const KeyData KEY_1_2 = {MEDIA_PLAY_PAUSE, true};
+const KeyData KEY_ENC_CW = {MEDIA_VOLUME_UP, true};
+const KeyData KEY_ENC_CCW = {MEDIA_VOLUME_DOWN, true};
+
+const int ENC_PIN_1 = 3;
+const int ENC_PIN_2 = 4;
 
 void setup()
 {
@@ -28,10 +38,32 @@ void setup()
     initializeCommands();
 }
 
+long oldPosition = -999;
+
 void loop()
 {
+    pinMode(ENC_PIN_1, INPUT_PULLUP);
+    pinMode(ENC_PIN_2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(ENC_PIN_1), checkPosition, CHANGE);
+
     readKeysFromCurrentArduino();
     delay(CMD_DELAY);
+}
+
+void checkPosition()
+{
+
+    if (digitalRead(ENC_PIN_1) == LOW)
+    {
+        if (digitalRead(ENC_PIN_2) == LOW)
+        {
+            sendEncoderCommand(true);
+        }
+        else
+        {
+            sendEncoderCommand(false);
+        }
+    }
 }
 
 void setInitialPinModes()
@@ -81,12 +113,50 @@ void sendCommand(int col, int row)
 {
     if (keys[col][row] == 0)
     {
-        Keyboard.press(KeyboardKeycode(commands[col][row]));
-        Consumer.press(ConsumerKeycode(commands[col][row]));
+        if (commands[col][row].media == true)
+        {
+            Consumer.press(ConsumerKeycode(commands[col][row].key));
+        }
+        else
+        {
+            Keyboard.press(KeyboardKeycode(commands[col][row].key));
+        }
     }
     else
     {
-        Keyboard.release(KeyboardKeycode(commands[col][row]));
-        Consumer.release(ConsumerKeycode(commands[col][row]));
+        if (commands[col][row].media == true)
+        {
+            Consumer.release(ConsumerKeycode(commands[col][row].key));
+        }
+        else
+        {
+            Keyboard.release(KeyboardKeycode(commands[col][row].key));
+        }
+    }
+}
+
+void sendEncoderCommand(bool isCW)
+{
+    if (isCW)
+    {
+        if (KEY_ENC_CW.media == true)
+        {
+            Consumer.write(ConsumerKeycode(KEY_ENC_CW.key));
+        }
+        else
+        {
+            Keyboard.write(KeyboardKeycode(KEY_ENC_CW.key));
+        }
+    }
+    else
+    {
+        if (KEY_ENC_CCW.media == true)
+        {
+            Consumer.write(ConsumerKeycode(KEY_ENC_CCW.key));
+        }
+        else
+        {
+            Keyboard.write(KeyboardKeycode(KEY_ENC_CCW.key));
+        }
     }
 }
