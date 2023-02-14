22b45ca61d01246a669e25af27055258e0cc848c
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-02-13 22:27:20 -0300

    Merge both sides to same file

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,137 +1,186 @@
+#include "Keypad.h"
+
 #include "Utils.h"
 #include "Wire.h"
 
+const boolean IS_LEFT_SIDE = true;
+
 // KEY MAPPINGS
-const Key KEY_0_0 = K_QUOTE;
-const Key KEY_0_1 = K_1;
-const Key KEY_0_2 = K_2;
-const Key KEY_0_3 = K_3;
-const Key KEY_0_4 = K_4;
-const Key KEY_0_5 = K_5;
+const _Key KEY_0_0 = K_QUOTE;
+const _Key KEY_0_1 = K_1;
+const _Key KEY_0_2 = K_2;
+const _Key KEY_0_3 = K_3;
+const _Key KEY_0_4 = K_4;
+const _Key KEY_0_5 = K_5;
 
-const Key KEY_1_0 = K_TAB;
-const Key KEY_1_1 = K_Q;
-const Key KEY_1_2 = K_W;
-const Key KEY_1_3 = K_E;
-const Key KEY_1_4 = K_R;
-const Key KEY_1_5 = K_T;
+const _Key KEY_1_0 = K_TAB;
+const _Key KEY_1_1 = K_Q;
+const _Key KEY_1_2 = K_W;
+const _Key KEY_1_3 = K_E;
+const _Key KEY_1_4 = K_R;
+const _Key KEY_1_5 = K_T;
 
-const Key KEY_2_0 = K_CAPS_LOCK;
-const Key KEY_2_1 = K_A;
-const Key KEY_2_2 = K_S;
-const Key KEY_2_3 = K_D;
-const Key KEY_2_4 = K_F;
-const Key KEY_2_5 = K_G;
+const _Key KEY_2_0 = K_CAPS_LOCK;
+const _Key KEY_2_1 = K_A;
+const _Key KEY_2_2 = K_S;
+const _Key KEY_2_3 = K_D;
+const _Key KEY_2_4 = K_F;
+const _Key KEY_2_5 = K_G;
 
-const Key KEY_3_0 = K_BACKSLASH;
-const Key KEY_3_1 = K_Z;
-const Key KEY_3_2 = K_X;
-const Key KEY_3_3 = K_C;
-const Key KEY_3_4 = K_V;
-const Key KEY_3_5 = K_B;
+const _Key KEY_3_0 = K_BACKSLASH;
+const _Key KEY_3_1 = K_Z;
+const _Key KEY_3_2 = K_X;
+const _Key KEY_3_3 = K_C;
+const _Key KEY_3_4 = K_V;
+const _Key KEY_3_5 = K_B;
 
-const Key KEY_4_0 = K_LEFT_SHIFT;
-const Key KEY_4_1 = K_LEFT_CTRL;
-const Key KEY_4_2 = K_LEFT_ALT;
-const Key KEY_4_3 = K_LEFT_WINDOWS;
-const Key KEY_4_4 = K_ENTER;
-const Key KEY_4_5 = K_SPACE;
+const _Key KEY_4_0 = K_LEFT_SHIFT;
+const _Key KEY_4_1 = K_LEFT_CTRL;
+const _Key KEY_4_2 = K_LEFT_ALT;
+const _Key KEY_4_3 = K_LEFT_WINDOWS;
+const _Key KEY_4_4 = K_ENTER;
+const _Key KEY_4_5 = K_SPACE;
 
 // PIN MAPPINGS
-byte cols[] = {5, 6, 7, 8, 9};
-byte rows[] = {19, 18, 15, 14, 16, 10};
+byte leftCols[] = {5, 6, 7, 8, 9};
+byte leftRows[] = {19, 18, 15, 14, 16, 10};
 
 char matrix[30];
 
 const int CMD_DELAY = 10;
 const uint32_t BAUD_RATE = 115200;
 
+const int LEFT_ROW_COUNT = sizeof(leftRows) / sizeof(leftRows[0]);
+const int LEFT_COL_COUNT = sizeof(leftCols) / sizeof(leftCols[0]);
+
+byte state_matrix[LEFT_COL_COUNT][LEFT_ROW_COUNT];
+_Key leftKeys[LEFT_COL_COUNT][LEFT_ROW_COUNT];
+
+// SECONDARY
+byte rows[] = {9, 8, 7, 6, 5, 4};
+byte cols[] = {18, 15, 14, 16, 10};
+
 const int ROW_COUNT = sizeof(rows) / sizeof(rows[0]);
 const int COL_COUNT = sizeof(cols) / sizeof(cols[0]);
 
-byte state_matrix[COL_COUNT][ROW_COUNT];
-Key keys[COL_COUNT][ROW_COUNT];
+char keys[ROW_COUNT][COL_COUNT] = {
+    {'0', '1', '2', '3', '4'},
+    {'5', '6', '7', '8', '9'},
+    {'a', 'b', 'c', 'd', 'e'},
+    {'f', 'g', 'h', 'i', 'j'},
+    {'k', 'l', 'm', 'n', 'o'},
+    {'p', 'q', 'r', 's', 't'},
+};
+
+Keypad keypad = Keypad(makeKeymap(keys), rows, cols, ROW_COUNT, COL_COUNT);
 
 void setup()
 {
-    Serial.begin(BAUD_RATE);
-    Wire.begin();
-    Wire.setClock(400000);
-    Keyboard.begin();
+    if (IS_LEFT_SIDE)
+    {
 
-    setInitialPinModes();
-    initializeCommands();
+        Serial.begin(BAUD_RATE);
+        Wire.begin();
+        Wire.setClock(400000);
+        Keyboard.begin();
+
+        setInitialPinModes();
+        initializeCommands();
+    }
+    else
+    {
+        Serial.begin(BAUD_RATE);
+        Wire.begin(8);
+        Wire.setClock(400000);
+        keypad.setDebounceTime(1);
+        Wire.onRequest(requestEvent);
+    }
+}
+
+void requestEvent()
+{
+    for (int i = 0; i < ROW_COUNT; i++)
+    {
+        Wire.write(keypad.bitMap[i]);
+    }
 }
 
 void loop()
 {
-    readKeysFromCurrentArduino();
-    readKeysFromSecondaryArduino();
-    delay(CMD_DELAY);
+    if (IS_LEFT_SIDE)
+    {
+        readKeysFromCurrentArduino();
+        readKeysFromSecondaryArduino();
+        delay(CMD_DELAY);
+    }
+    else
+    {
+        keypad.getKeys();
+    }
 }
 
 void setInitialPinModes()
 {
-    for (int x = 0; x < ROW_COUNT; x++)
+    for (int x = 0; x < LEFT_ROW_COUNT; x++)
     {
-        pinMode(rows[x], INPUT);
+        pinMode(leftRows[x], INPUT);
     }
 
-    for (int x = 0; x < COL_COUNT; x++)
+    for (int x = 0; x < LEFT_COL_COUNT; x++)
     {
-        pinMode(cols[x], INPUT_PULLUP);
+        pinMode(leftCols[x], INPUT_PULLUP);
     }
 }
 
 void initializeCommands()
 {
-    keys[0][0] = KEY_0_0;
-    keys[0][1] = KEY_0_1;
-    keys[0][2] = KEY_0_2;
-    keys[0][3] = KEY_0_3;
-    keys[0][4] = KEY_0_4;
-    keys[0][5] = KEY_0_5;
+    leftKeys[0][0] = KEY_0_0;
+    leftKeys[0][1] = KEY_0_1;
+    leftKeys[0][2] = KEY_0_2;
+    leftKeys[0][3] = KEY_0_3;
+    leftKeys[0][4] = KEY_0_4;
+    leftKeys[0][5] = KEY_0_5;
 
-    keys[1][0] = KEY_1_0;
-    keys[1][1] = KEY_1_1;
-    keys[1][2] = KEY_1_2;
-    keys[1][3] = KEY_1_3;
-    keys[1][4] = KEY_1_4;
-    keys[1][5] = KEY_1_5;
+    leftKeys[1][0] = KEY_1_0;
+    leftKeys[1][1] = KEY_1_1;
+    leftKeys[1][2] = KEY_1_2;
+    leftKeys[1][3] = KEY_1_3;
+    leftKeys[1][4] = KEY_1_4;
+    leftKeys[1][5] = KEY_1_5;
 
-    keys[2][0] = KEY_2_0;
-    keys[2][1] = KEY_2_1;
-    keys[2][2] = KEY_2_2;
-    keys[2][3] = KEY_2_3;
-    keys[2][4] = KEY_2_4;
-    keys[2][5] = KEY_2_5;
+    leftKeys[2][0] = KEY_2_0;
+    leftKeys[2][1] = KEY_2_1;
+    leftKeys[2][2] = KEY_2_2;
+    leftKeys[2][3] = KEY_2_3;
+    leftKeys[2][4] = KEY_2_4;
+    leftKeys[2][5] = KEY_2_5;
 
-    keys[3][0] = KEY_3_0;
-    keys[3][1] = KEY_3_1;
-    keys[3][2] = KEY_3_2;
-    keys[3][3] = KEY_3_3;
-    keys[3][4] = KEY_3_4;
-    keys[3][5] = KEY_3_5;
+    leftKeys[3][0] = KEY_3_0;
+    leftKeys[3][1] = KEY_3_1;
+    leftKeys[3][2] = KEY_3_2;
+    leftKeys[3][3] = KEY_3_3;
+    leftKeys[3][4] = KEY_3_4;
+    leftKeys[3][5] = KEY_3_5;
 
-    keys[4][0] = KEY_4_0;
-    keys[4][1] = KEY_4_1;
-    keys[4][2] = KEY_4_2;
-    keys[4][3] = KEY_4_3;
-    keys[4][4] = KEY_4_4;
-    keys[4][5] = KEY_4_5;
+    leftKeys[4][0] = KEY_4_0;
+    leftKeys[4][1] = KEY_4_1;
+    leftKeys[4][2] = KEY_4_2;
+    leftKeys[4][3] = KEY_4_3;
+    leftKeys[4][4] = KEY_4_4;
+    leftKeys[4][5] = KEY_4_5;
 }
 
 void readKeysFromCurrentArduino()
 {
-    for (int colIndex = 0; colIndex < COL_COUNT; colIndex++)
+    for (int colIndex = 0; colIndex < LEFT_COL_COUNT; colIndex++)
     {
-        byte curCol = cols[colIndex];
+        byte curCol = leftCols[colIndex];
         pinMode(curCol, OUTPUT);
         digitalWrite(curCol, LOW);
 
-        for (int rowIndex = 0; rowIndex < ROW_COUNT; rowIndex++)
+        for (int rowIndex = 0; rowIndex < LEFT_ROW_COUNT; rowIndex++)
         {
-            byte rowCol = rows[rowIndex];
+            byte rowCol = leftRows[rowIndex];
             pinMode(rowCol, INPUT_PULLUP);
             state_matrix[colIndex][rowIndex] = digitalRead(rowCol);
             sendCommand(colIndex, rowIndex);
@@ -157,10 +206,10 @@ void sendCommand(int col, int row)
 {
     if (state_matrix[col][row] == 0)
     {
-        keys[col][row].press();
+        leftKeys[col][row].press();
     }
     else
     {
-        keys[col][row].release();
+        leftKeys[col][row].release();
     }
 }
