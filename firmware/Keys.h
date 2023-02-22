10ba0f0178f9f5fb0d946132c678bdf2699f73e7
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-02-22 19:43:05 -0300

    Change keys

--- firmware/Keys.h
+++ firmware/Keys.h
@@ -1,7 +1,7 @@
 const MultiKey L_MOD = {1};
 const MultiKey R_MOD = {2};
 
-const MultiKey L_KEY_0_0 = {K_TILDE, K_F1};
+const MultiKey L_KEY_0_0 = {K_ESC, K_F1};
 const MultiKey L_KEY_0_1 = {K_1, K_F2};
 const MultiKey L_KEY_0_2 = {K_2, K_F3};
 const MultiKey L_KEY_0_3 = {K_3, K_F4};
@@ -16,7 +16,7 @@ const MultiKey L_KEY_1_4 = {K_R};
 const MultiKey L_KEY_1_5 = {K_T};
 
 const MultiKey L_KEY_2_0 = {K_LEFT_SHIFT};
-const MultiKey L_KEY_2_1 = {K_A};
+const MultiKey L_KEY_2_1 = {K_A, K_QUOTE};
 const MultiKey L_KEY_2_2 = {K_S};
 const MultiKey L_KEY_2_3 = {K_D};
 const MultiKey L_KEY_2_4 = {K_F};
@@ -25,7 +25,7 @@ const MultiKey L_KEY_2_5 = {K_G};
 const MultiKey L_KEY_3_0 = L_MOD;
 const MultiKey L_KEY_3_1 = {K_Z};
 const MultiKey L_KEY_3_2 = {K_X};
-const MultiKey L_KEY_3_3 = {K_C};
+const MultiKey L_KEY_3_3 = {K_C, K_SEMICOLON};
 const MultiKey L_KEY_3_4 = {K_V};
 const MultiKey L_KEY_3_5 = {K_B};
 
@@ -44,28 +44,28 @@ const MultiKey R_KEY_0_3 = {K_9, K_F10};
 const MultiKey R_KEY_0_4 = {K_0, K_F11};
 const MultiKey R_KEY_0_5 = {K_ESC, K_F12};
 
-const MultiKey R_KEY_1_0 = {K_Y};
-const MultiKey R_KEY_1_1 = {K_U};
+const MultiKey R_KEY_1_0 = {K_Y, K_MINUS};
+const MultiKey R_KEY_1_1 = {K_U, K_EQUAL};
 const MultiKey R_KEY_1_2 = {K_I};
-const MultiKey R_KEY_1_3 = {K_O, K_LEFT_BRACE};
-const MultiKey R_KEY_1_4 = {K_P, K_RIGHT_BRACE};
-const MultiKey R_KEY_1_5 = {K_QUOTE, K_VOLUME_UP};
+const MultiKey R_KEY_1_3 = {K_O, K_RIGHT_BRACE};
+const MultiKey R_KEY_1_4 = {K_P, K_BACKSLASH};
+const MultiKey R_KEY_1_5 = {K_TILDE, K_VOLUME_UP};
 
 const MultiKey R_KEY_2_0 = {K_H, K_LEFT_ARROW};
 const MultiKey R_KEY_2_1 = {K_J, K_DOWN_ARROW};
 const MultiKey R_KEY_2_2 = {K_K, K_UP_ARROW};
 const MultiKey R_KEY_2_3 = {K_L, K_RIGHT_ARROW};
-const MultiKey R_KEY_2_4 = {K_SEMICOLON};
+const MultiKey R_KEY_2_4 = {K_SLASH};
 const MultiKey R_KEY_2_5 = {K_RIGHT_SHIFT, K_VOLUME_DOWN};
 
 const MultiKey R_KEY_3_0 = {K_N};
 const MultiKey R_KEY_3_1 = {K_M};
-const MultiKey R_KEY_3_2 = {K_COMMA};
+const MultiKey R_KEY_3_2 = {K_COMMA, K_LEFT_BRACE};
 const MultiKey R_KEY_3_3 = {K_PERIOD};
-const MultiKey R_KEY_3_4 = {K_SLASH};
+const MultiKey R_KEY_3_4 = {K_NON_US};
 const MultiKey R_KEY_3_5 = R_MOD;
 
-const MultiKey R_KEY_4_0 = R_MOD;
+const MultiKey R_KEY_4_0 = {K_ESC};
 const MultiKey R_KEY_4_1 = {K_BACKSPACE, K_DELETE};
 const MultiKey R_KEY_4_2 = {K_SPACE};
 const MultiKey R_KEY_4_3 = {K_RIGHT_ALT};
