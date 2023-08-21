cd5c44a3f009c96ee0774babffdbe57db92ff0e3
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-08-21 13:12:10 -0300

    Change pin layout

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,25 +4,25 @@
 
 #include "Device.h"
 #include "Utils.h"
-#include "Keys.h"
+#include "KeyMapping.h"
 
 Device dev = Device("Kovu Keyboard", "Mono", "1");
 
 // PIN MAPPINGS
 const byte L_ROW_PINS[] = {5, 6, 7, 8, 9};
 const byte L_COL_PINS[] = {19, 18, 15, 14, 16, 10};
-const byte R_ROW_PINS[] = {18, 15, 14, 16, 10};
-const byte R_COL_PINS[] = {9, 8, 7, 6, 5, 4};
+
+const byte R_ROW_PINS[] = {5, 6, 7, 8, 9};
+const byte R_COL_PINS[] = {19, 18, 15, 14, 16, 10};
 
 const uint32_t ROW_COUNT = sizeof(L_ROW_PINS) / sizeof(L_ROW_PINS[0]);
 const uint32_t COL_COUNT = sizeof(L_COL_PINS) / sizeof(L_COL_PINS[0]);
 
-typedef MultiKey Keys[ROW_COUNT][COL_COUNT];
+typedef MultiKey KeyMatrix[ROW_COUNT][COL_COUNT];
 
 const uint32_t BAUD_RATE = 115200;
 const uint32_t I2C_CLOCK = 400000;
 
-byte keysStateBuffer[COL_COUNT];
 unsigned long startTime = 0;
 byte layer = 0;
 
@@ -30,7 +30,7 @@ byte layer = 0;
 
 const uint32_t DEBOUNCE_TIME = 10;
 
-Keys lKeys = {
+KeyMatrix lKeys = {
     {L_KEY_0_0, L_KEY_0_1, L_KEY_0_2, L_KEY_0_3, L_KEY_0_4, L_KEY_0_5},
     {L_KEY_1_0, L_KEY_1_1, L_KEY_1_2, L_KEY_1_3, L_KEY_1_4, L_KEY_1_5},
     {L_KEY_2_0, L_KEY_2_1, L_KEY_2_2, L_KEY_2_3, L_KEY_2_4, L_KEY_2_5},
@@ -38,7 +38,7 @@ Keys lKeys = {
     {L_KEY_4_0, L_KEY_4_1, L_KEY_4_2, L_KEY_4_3, L_KEY_4_4, L_KEY_4_5},
 };
 
-Keys rKeys = {
+KeyMatrix rKeys = {
     {R_KEY_0_0, R_KEY_0_1, R_KEY_0_2, R_KEY_0_3, R_KEY_0_4, R_KEY_0_5},
     {R_KEY_1_0, R_KEY_1_1, R_KEY_1_2, R_KEY_1_3, R_KEY_1_4, R_KEY_1_5},
     {R_KEY_2_0, R_KEY_2_1, R_KEY_2_2, R_KEY_2_3, R_KEY_2_4, R_KEY_2_5},
@@ -49,6 +49,7 @@ Keys rKeys = {
 #else
 
 const uint32_t DEBOUNCE_TIME = 1;
+byte keysStateBuffer[COL_COUNT];
 
 #endif
 
@@ -81,12 +82,6 @@ void loop()
 #endif
 }
 
-void sendKeysStateToMaster()
-{
-    for (int c = 0; c < COL_COUNT; c++)
-        Wire.write(keysStateBuffer[c]);
-}
-
 void scanKeys(Size size, const byte rows[], const byte cols[], void (*cb)(Coords c, boolean pressed))
 {
     for (byte c = 0; c < size.columns; c++)
@@ -107,7 +102,7 @@ void scanKeys(Size size, const byte rows[], const byte cols[], void (*cb)(Coords
 
 void sendCommand(Coords c, boolean pressed, boolean isLeft)
 {
-    Keys &keys = isLeft ? lKeys : rKeys;
+    KeyMatrix &keys = isLeft ? lKeys : rKeys;
     if (pressed)
     {
         keys[c.row][c.col].press(layer);
@@ -130,19 +125,27 @@ void rSendCommand(Coords c, boolean pressed)
 
 void readKeysStateFromSlave(void (*cb)(Coords c, boolean pressed))
 {
-    Wire.requestFrom(8, ROW_COUNT, true);
+    Wire.requestFrom(8, ROW_COUNT, false);
 
-    for (byte r = 0; r < ROW_COUNT; r++)
+    byte r = 0;
+    while (Wire.available())
     {
         byte b = Wire.read();
         for (byte c = 0; c < COL_COUNT; c++)
         {
             cb({r, c}, bitRead(b, c));
         }
+        r++;
     }
 }
 
 #else
+
+void sendKeysStateToMaster()
+{
+    for (int c = 0; c < COL_COUNT; c++)
+        Wire.write(keysStateBuffer[c]);
+}
 
 void sendToBuffer(Coords c, boolean pressed)
 {
